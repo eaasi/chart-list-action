ea786ee88ac7ab49d2338a7f7e18bfcec9e0241c
Oleg Stobbe <oleg@bitforr.com>
2025-03-17 20:47:55 +0100

    feat: allow behaviour tweaking via 'if-no-charts-found'

--- src/constants.ts
+++ src/constants.ts
@@ -11,9 +11,20 @@ export const CHART_SECTION_HEADING = 'Charts to be processed:';
 export const CHART_ENTRY_REGEXP =
     /^\s*(?<name>[\w-]+) => \(version: "(?<version>.+)", path: "(?<path>.+)"\)/;
 
+/** Message reported if no charts are found */
+export const NO_CHARTS_FOUND_MESSAGE = 'No charts found!';
+
+/** Desired behaviour if no charts are found */
+export enum NotFoundBehaviour {
+  IGNORE = 'ignore',
+  ERROR = 'error',
+  WARN = 'warn',
+}
+
 /** Action's input name */
 export enum InputName {
   CT_LOG_FILE = 'ct-log-file',
+  IF_NO_CHARTS_FOUND = 'if-no-charts-found',
 }
 
 /** Action's output name */
